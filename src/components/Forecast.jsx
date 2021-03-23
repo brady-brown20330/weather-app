0e4acbf356ed2c05d1e6d8a5aef8b69a2d804fed
Brady Brown <brownb202@gmail.com>
2021-03-22 18:41:28 -0600

    app now displays weekly forecast

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,9 +2,32 @@ import React from "react";
 
 const Forecast = (props) => {
   console.log(props.cityWeather)
+
+  if (!props.cityWeather.consolidated_weather) {
+    return (
+      <div></div>
+    )
+  }
   return (
-    <div>{props.cityWeather.consolidated_weather ? `${props.cityWeather.parent.title} is currently experiencing ${props.cityWeather.consolidated_weather[0].weather_state_name}` : null}</div>
+    // <div>{props.cityWeather.consolidated_weather ? `${props.cityWeather.parent.title} is currently experiencing ${props.cityWeather.consolidated_weather[0].weather_state_name}` : null}</div>
+    <div>
+      <div>{`Weather for the ${props.cityWeather.location_type} of ${props.cityWeather.title}, ${props.cityWeather.parent.title}`}</div>
+      <DailyForecast cityWeather={props.cityWeather}/>
+    </div>
   )
 }
 
+
 export default Forecast;
+
+const DailyForecast = (props) => {
+  console.log('props in the daily forecast: ', props.cityWeather.consolidated_weather)
+  return props.cityWeather.consolidated_weather.map((date) => {
+      return (
+        <div className='date-block' key={date.id}>
+          <div>{date.applicable_date}</div>
+          <div>{date.weather_state_name}</div>
+        </div>
+      )
+    })
+}
