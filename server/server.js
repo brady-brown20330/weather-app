0e2d6c68013d16d1e191e3f7b538b4f511a68464
Brady Brown <brownb202@gmail.com>
2021-03-22 20:49:27 -0600

    modified res.headers

--- server/server.js
+++ server/server.js
@@ -3,42 +3,24 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
-var cors = require('cors')
 
-var whitelist = ['https://stupefied-blackwell-da7fbd.netlify.app/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // ENDPOINT: searches for cities that match the input field
 app.get('https://www.metaweather.com/api/location/search/?query=', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   const url = `https://www.metaweather.com/api/location/search/?query=${req.query.data}`
-  return axios.get(url, {
-    headers: {
-      'Access-Control-Allow-Origin':'*'
-    }
-  })
+  return axios.get(url)
   .then(data => res.send(data.data))
 })
 
 // ENDPOINT: gets weather for a given city based on woeid
 app.get('https://www.metaweather.com/api/location/', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   const url = `https://www.metaweather.com/api/location/${req.query.data}/`
-  return axios.get(url, {
-    headers: {
-      'Access-Control-Allow-Origin':'*'
-    }
-  })
+  return axios.get(url)
   .then(data => res.send(data.data))
 })
 
