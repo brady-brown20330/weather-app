f2f7aff52de1a362d67fdb1e22f338af5ce37e6e
Brady Brown <brownb202@gmail.com>
2021-03-22 20:43:41 -0600

    added cors options

--- server/server.js
+++ server/server.js
@@ -4,7 +4,19 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
 var cors = require('cors')
-app.use(cors())
+
+var whitelist = ['https://60595473dd7e650007f5ccd3--stupefied-blackwell-da7fbd.netlify.app/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 
 // ENDPOINT: searches for cities that match the input field
 app.get('https://www.metaweather.com/api/location/search/?query=', (req, res) => {
