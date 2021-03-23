b54ee6074ee3e19cceff1b6a312cc2af3d6f2903
Brady Brown <brownb202@gmail.com>
2021-03-22 19:18:37 -0600

    added timezone and h1 tag

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -10,7 +10,7 @@ const Forecast = (props) => {
   return (
     // <div>{props.cityWeather.consolidated_weather ? `${props.cityWeather.parent.title} is currently experiencing ${props.cityWeather.consolidated_weather[0].weather_state_name}` : null}</div>
     <div>
-      <div>{`Weather for the ${props.cityWeather.location_type} of ${props.cityWeather.title}, ${props.cityWeather.parent.title}`}</div>
+      <h1>{`Weather for the ${props.cityWeather.location_type} of ${props.cityWeather.title}, ${props.cityWeather.parent.title} (${props.cityWeather.timezone_name})`}</h1>
       <DailyForecast cityWeather={props.cityWeather}/>
     </div>
   )
@@ -25,7 +25,7 @@ const DailyForecast = (props) => {
       return (
         <div className='date-block' key={date.id}>
           <div>{date.applicable_date}</div>
-          <div>{date.weather_state_name}</div>
+          <div>{`${date.weather_state_name} ${date.predictability}% chance`}</div>
           <div>{`Wind from the ${date.wind_direction_compass} at ${Math.round(date.wind_speed)} MPH`}</div>
           <div>{`Visibility ${Math.round(date.visibility)} miles`}</div>
         </div>
